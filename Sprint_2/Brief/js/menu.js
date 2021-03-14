e87b242c213b78ccfcc5e5026c58b87c69f8c9dc
Said <zitouni.sd@gmail.com>
2021-03-14 16:09:51 +0100

    footer problem resolved need to work more on js

--- Sprint_2/Brief/js/menu.js
+++ Sprint_2/Brief/js/menu.js
@@ -1,25 +1,27 @@
 const panels = document.querySelectorAll(".products-panel");
 
 
-function stretch(){
+function stretch() {
     this.classList.toggle("stretch");
 }
-function appear(){
+
+function appear() {
     this.classList.toggle("appear")
 }
-function backToNormal(){
-    this.classList.remove("stretch");
-    this.classList.remove("appear");
+
+function backToNormal() {
+    this.classList.toggle("stretch");
+    this.classList.toggle("appear");
 }
 
 panels.forEach(
-    function(panel){
-        panel.addEventListener('mouseenter',stretch);
-        panel.addEventListener('mouseenter',stretch);
+    function(panel) {
+        panel.addEventListener('mouseenter', stretch);
+        panel.addEventListener('mouseenter', stretch);
     }
 )
 panels.forEach(
-    function(panel){
-        panel.addEventListener('mouseout',backToNormal);
+    function(panel) {
+        panel.addEventListener('mouseout', backToNormal);
     }
 )
