5baae0ed6f235574c1ca79c4d91510932cde9d14
Said <zitouni.sd@gmail.com>
2021-04-16 15:20:05 +0000

    start form

---
+++ sprint3/workshops/form/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Youcode</a>
+    <div class="d-flex justify-content-between collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <router-link class="nav-link pr-3" to="/signin">sign in</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link pr-3" to="/">sign up</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="nav" id="nav"></div>
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
